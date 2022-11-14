f160f27ed1f1675012f865e9842862ba0e21e78d
jeromecox <jeromecox@hotmail.com>
2022-11-14 16:27:56 -0600

    all buttons except EQUAL work

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-let buttons = document.querySelectorAll(".for-display");
+let buttons = document.querySelectorAll(".button");
 let display = document.querySelector("#result");
 let clear = document.querySelector("#clear");
+let equal = document.querySelector("#equals");
 
-// for (let for-display of buttons) {
-//   buttons.addEventListener("click", function (event) {
-//     display.innerText += event.target.innerText;
-//   })
-// }
+for (let button of buttons) {
+  button.addEventListener("click", function (event) {
+    display.innerText += `${event.target.innerText}`;
+  });
+}
+
+// clear button works
 clear.addEventListener("click", function (event) {
   display.innerText = "";
+});
+
+equal.addEventListener("click", function (event) {
+  // doing this now to make EQUAL click not display equal character
+  display.innerText = "different";
 });
